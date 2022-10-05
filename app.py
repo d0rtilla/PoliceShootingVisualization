e6c92ce5158f8f98c008ad83043363a5804c82a7
Jae Neuharth <Jae.neuharth@gmail.com>
2022-10-04 21:19:47 -0500

    added db reading functionality to homepage

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 # Import Dependencies
-from flask import Flask
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
 
 # Create Flask app
 app = Flask(__name__)
 
+mongo = PyMongo(app, uri = 'mongodb://localhost:27017/USPoliceShootingData')
 
 # Create Flask routes
 ################################
 # Homepage
 @app.route("/")
 def homepage():
-    return "Hello, world."
+    shootingData = mongo.db.PoliceShootingData
+    return render_template('index.html', shootingData=shootingData)
 
 # First visualization page - map
 # @app.route("/map")
