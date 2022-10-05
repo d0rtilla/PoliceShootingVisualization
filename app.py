7ecac211a9f2d57cb18362a94c9601552fcf0b5b
Sam Espe <sam.espe.sci@gmail.com>
2022-10-04 20:33:54 -0500

    Created basic skeleton for Flask app

---
+++ app.py
@@ -0,0 +1,33 @@
+# Import Dependencies
+from flask import Flask
+
+# Create Flask app
+app = Flask(__name__)
+
+
+# Create Flask routes
+################################
+# Homepage
+# @app.route("/")
+# def homepage():
+
+# First visualization page - map
+# @app.route("/map")
+# def map():
+
+# Second visualization page - bubble chart
+# @app.route("/bubble_chart")
+# def bubble_chart():
+
+# Third visualization page - pie charts
+# @app.route("/pie_charts")
+# def pie_charts:
+
+# Discussion page
+# @app.route("/discussion")
+# def discussion():
+
+
+# End Flask bit
+if __name__ == '__main__':
+    app.run(debug=True)
