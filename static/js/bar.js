0efafad91a25848e0bb5bd175fcd72e7501fa4a5
Dortilla <jae.neuharth@gmail.com>
2022-10-13 21:31:05 -0500

    moved js from bar.html to bar.js

--- static/js/bar.js
+++ static/js/bar.js
@@ -13,42 +13,42 @@ d3.json(mongo_endpoint).then(function(shootingData){
     };
 
 
-    const labels = ["Here", "Are", "A", "Set", "Of", "June"];
-    const data = {
-        labels: labels,
-        datasets: [
-        {
-            label: "My First dataset",
-            backgroundColor: "rgb(255, 99, 132)",
-            borderColor: "rgb(255, 99, 132)",
-            data: [
-            {
-                x: 5,
-                y: 3,
-                r: 19,
-            },
-            {
-                x: 8,
-                y: 2,
-                r: 80,
-            },
-            {
-                x: 6.5,
-                y: 2.5,
-                r: 46,
-            },
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
             ],
-        },   
-        ],
-    };
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
 
-    const config = {
-    type: "bubble",
-    data: data,
-    options: {},
-    };
-
-    const myChart = new Chart(document.getElementById("myChart"), config);
 })
         
 
