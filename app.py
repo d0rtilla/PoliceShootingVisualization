a7a87a277e3f7ceb7721662005146ea6fb19c851
Alan Jallah <alanbjallah@gmail.com>
2022-10-13 21:14:56 -0500

    changed bubble chart to bar chart

--- app.py
+++ app.py
@@ -6,20 +6,25 @@ from itsdangerous import json
 # Create Flask app
 app = Flask(__name__)
 
-mongo = PyMongo(app, uri = 'mongodb://localhost:27017/USPoliceShootingData')
+mongo = PyMongo(app, uri='mongodb://localhost:27017/USPoliceShootingData')
 
 # Create Flask routes
 ################################
 # Homepage
+
+
 @app.route("/")
 def homepage():
-    shootingData = mongo.db.PoliceShootingData.find({},{'_id':0})
+    shootingData = mongo.db.PoliceShootingData.find({}, {'_id': 0})
     return render_template('index.html', shootingData=shootingData)
 
 # Route to get data from MongoDB
+
+
 @app.route("/mongo")
 def readMongo():
-    data = mongo.db.PoliceShootingData.find({},{'_id':0, 'latitude':1, 'longitude':1, 'is_geocoding_exact':1, 'name':1, 'date':1, 'age':1, 'sex':1, 'ethnicity':1, 'armed':1, 'city':1, 'state':1})
+    data = mongo.db.PoliceShootingData.find({}, {'_id': 0, 'latitude': 1, 'longitude': 1, 'is_geocoding_exact': 1,
+                                            'name': 1, 'date': 1, 'age': 1, 'sex': 1, 'ethnicity': 1, 'armed': 1, 'city': 1, 'state': 1})
     result = []
     for item in data:
         result.append(item)
@@ -27,35 +32,44 @@ def readMongo():
     return jsonify(result)
 
 # First visualization page - map
+
+
 @app.route("/map")
 def map():
     return render_template('map.html')
 
 # Second visualization page - bubble chart
-@app.route("/bubble_chart")
+
+
+@app.route("/bar_chart")
 def bubble_chart():
-    shootingData = mongo.db.PoliceShootingData.find({},{'_id':0,'armed':1})
+    shootingData = mongo.db.PoliceShootingData.find({}, {'_id': 0, 'armed': 1})
     result = shootingData
-    return render_template('bubble_chart.html', result = result)
+    return render_template('bar_chart.html', result=result)
 
 
 # Third visualization page - pie charts
 @app.route("/pie_charts")
-def pie_charts():                                                  
-    shootingData = mongo.db.PoliceShootingData.find( {},{'_id':0, 'age':1,'ethnicity':1,'sex':1})
+def pie_charts():
+    shootingData = mongo.db.PoliceShootingData.find(
+        {}, {'_id': 0, 'age': 1, 'ethnicity': 1, 'sex': 1})
     result = shootingData
-    return render_template('pie_charts.html', result = result)
+    return render_template('pie_charts.html', result=result)
 
 # Discussion page
+
+
 @app.route("/discussion")
 def discussion():
     return render_template('discussion.html')
 
+
 @app.route('/data')
 def data():
-    shootingData = mongo.db.PoliceShootingData.find({},{'_id':False})
+    shootingData = mongo.db.PoliceShootingData.find({}, {'_id': False})
     result = shootingData
-    return render_template('data.html', result = result)
+    return render_template('data.html', result=result)
+
 
 # End Flask bit
 if __name__ == '__main__':
